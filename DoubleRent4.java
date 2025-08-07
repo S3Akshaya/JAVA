ab3683e6c173b11f84c57f18ac55db07c8f08c71
S3Akshaya <126939340+S3Akshaya@users.noreply.github.com>
2025-08-07 16:30:32 +0530

    Create DoubleRent4.java

---
+++ DoubleRent4.java
@@ -0,0 +1,20 @@
+/* Chefina decided to move into Chef's apartment. Chef was initially paying a rent of  X rupees. Since Chefina is moving in, the owner decided to double the rent.  
+Find the final rent Chef needs to pay.  3 rupees. After Chefina moves in, he needs to pay 
+2⋅ 3 = 6  rupees. */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner a = new Scanner(System.in);
+		int b = a.nextInt();
+		int c = b*2;
+		System.out.println(c);
+
+	}
+}
