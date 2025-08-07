14e3c2d209bdc4afd7087a95c7a62396b3e8c1da
S3Akshaya <126939340+S3Akshaya@users.noreply.github.com>
2025-08-07 16:02:01 +0530

    Create LuckySeven.java

---
+++ LuckySeven.java
@@ -0,0 +1,24 @@
+/* Chef considers the number 7 lucky. As a result, he believes that the  7-th letter he sees on a day is his lucky letter of the day.
+You are given a string S of length 10.
+What is Chef's lucky letter?
+
+input   -  proceeding
+output -   d
+
+*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner a=new Scanner(System.in);
+		String b=a.nextLine();
+		System.out.println(b.charAt(6));
+
+	}
+}
